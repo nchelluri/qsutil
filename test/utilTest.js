7a0a4437c7aba4ed71cb08a4d3eef1245c8aba6a
Narsimham Chelluri <nchelluri.public+git.commit@gmail.com>
2014-05-11 01:30:11 -0700

    Added another test

--- test/utilTest.js
+++ test/utilTest.js
@@ -8,4 +8,10 @@
             propEqual(Util.queryStringParams('asdf=123&xyz=789'), { asdf: '123', xyz: '789' }, 'properties as expected');
         });
     })();
+
+    (function() {
+        test('buildQueryString: Builds a QS from a hash of string-key-to-string-value pairs', function() {
+            equal(Util.buildQueryString({ asdf: 123, xyz: 789 }), '?asdf=123&xyz=789', 'string as expected');
+        });
+    })();
 })();
